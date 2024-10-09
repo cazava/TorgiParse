981f7d6e73e2521dce8727b36207e5f8e31dfe37
cazavas <cazavas@gmail.com>
2024-10-09 16:07:43 +0300

    0.1

--- d.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-
-def get_map_urt(latitude, longitude, api_map):
-    api = "bfb6d087-79d4-406f-ae25-c704ef5bc87b"
-    url_map = f'''https://static-maps.yandex.ru/v1?ll={latitude},{longitude}&size=600,450&z=13&pt={latitude},{longitude}&apikey=bfb6d087-79d4-406f-ae25-c704ef5bc87b'''
-    return url_map
-
